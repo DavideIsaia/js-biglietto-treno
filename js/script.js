ce0d000dfc802368849a153310b4888fb9f3ee93
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-07 14:32:28 +0200

    inserito ciclo if else

--- js/script.js
+++ js/script.js
@@ -8,8 +8,17 @@ const ageNumber = parseInt(prompt('Inserisci la tua età'));
 console.log(ageNumber, typeof (ageNumber));
 
 // moltiplico i km inseriti per 0.21
+let ticketPrice = kmNumber * 0.21;
+console.log(ticketPrice, typeof (ticketPrice));
 
 // se l'età è compresa tra 18 e 65 mostro il risultato, altrimenti se l'età è inferiore a 18, tolgo il 20% dal risultato, altrimenti se l'età è superiore a 65 tolgo il 40% dal risultato.
+if (ageNumber >= 18 && ageNumber < 65) {
+    console.log(string = "tariffa normale");
+} else if (ageNumber < 18) {
+    console.log(string = "minorenne");
+} else if (ageNumber > 65) {
+    console.log(string = "senior");
+}
 
 // tolgo i decimali superflui dal risultato
 
