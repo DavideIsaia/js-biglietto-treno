89b9f5025ccf6b8a7a125d6cb73d577ace29ca4e
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-07 14:06:25 +0200

    inserito ragionamento logico

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const funge = "funziona";
-console.log(funge);
+// chiedo all'utente il numero di km che percorrerà
+const
+
+// chiedo l'età
+
+// moltiplico i km inseriti per 0.21
+
+// se l'età è compresa tra 18 e 65 mostro il risultato, altrimenti se l'età è inferiore a 18, tolgo il 20% dal risultato, altrimenti se l'età è superiore a 65 tolgo il 40% dal risultato.
+
+// tolgo i decimali superflui dal risultato
+
+// mostro il risultato del prezzo nell'html
