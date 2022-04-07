61d5413765d49d819c8943e4ba656c5d63e76261
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-07 15:13:17 +0200

    aggiunta visualizzazione km e tariffa nell'html

--- js/script.js
+++ js/script.js
@@ -12,15 +12,19 @@ console.log(price, typeof (price));
 
 // se l'età è compresa tra 18 e 65 mostro il risultato, altrimenti se l'età è inferiore a 18, tolgo il 20% dal risultato, altrimenti se l'età è superiore a 65 tolgo il 40% dal risultato.
 let discount;
+let fare;
 
 if (ageNumber >= 18 && ageNumber < 65) {
-    console.log(string = "tariffa normale");
+    // console.log(string = "tariffa normale");
+    fare = (string = "tariffa normale");
     discount = 0;
 } else if (ageNumber < 18) {
     console.log(string = "minorenne");
+    fare = (string = "tariffa minorenni");
     discount = (price * 20) / 100;
 } else if (ageNumber > 65) {
     console.log(string = "senior");
+    fare = (string = "tariffa anziani");
     discount = (price * 40) / 100;
 }
 // sottraggo lo sconto, se presente, dal prezzo pieno
@@ -32,6 +36,5 @@ const ticketPriceEuro = ticketPrice.toFixed(2);
 console.log(ticketPriceEuro);
 
 // mostro il risultato del prezzo nell'html
-const message = `Il prezzo del biglietto è: 
-${ticketPriceEuro} €`;
+const message = `Il prezzo del biglietto per ${kmNumber}Km con ${fare} è di: ${ticketPriceEuro} €`;
 document.getElementById('ticket-price').innerHTML = message;
